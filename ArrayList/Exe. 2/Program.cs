88893c1f1ff1debc3f45d46c5251f976896f0542
Gustavo Ribeiro Pena <gustavorpena02@gmail.com>
2025-11-06 10:17:29 -0300

    Exe 2 Feito

--- ArrayList/Exe. 2/Program.cs
+++ ArrayList/Exe. 2/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿// Crie um ArrayList que contenha valores de tipos diferentes (int, string, double).
+// Percorra o ArrayList e exiba apenas os números inteiros. Depois, exiba apenas as strings.
 
-namespace MyApp
+using System;
+using System.Collections;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        ArrayList lista = new ArrayList();
+
+        lista.Add(10);
+        lista.Add("Gustavo");
+        lista.Add(25.7);
+        lista.Add(42);
+        lista.Add("Pena");
+
+        Console.WriteLine("Números inteiros:");
+        foreach (var item in lista)
         {
-            
+            if (item is int)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        Console.WriteLine("\nStrings:");
+        foreach (var item in lista)
+        {
+            if (item is string)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
